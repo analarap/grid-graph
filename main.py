ea7cbaf35d621d9890532408379bea821bdba348
analarap <analarapupo@gmail.com>
2025-02-09 18:47:31 -0300

    fix: exception of grid empty fixed

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from graph_builder import build_graph
 from graphviz_output import export_to_graphviz
 
 def load_grid(filename):
-    """Carrega a grade de um arquivo de texto."""
-    with open(filename, 'r') as f:
-        return [list(line.strip()) for line in f.readlines()]
+    with open(filename, "r") as f:
+        grid = [list(line.strip()) for line in f.readlines() if line.strip()]
+    return grid
 
 def main():
     grid = load_grid("example_grid.txt")
